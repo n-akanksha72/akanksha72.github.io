ed2d6680249cd764dbc71e3582476a339f433d80
Akanksha <nakanksha72@gmail.com>
2022-08-26 16:11:40 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,13 +1,13 @@
 var names=new Array();
-names[0]="Yukta";
+names[0]="Yaakov";
 names[1]="John";
-names[2]="Jay";
+names[2]="Jen";
 names[3]="jason";
-names[4]="priya";
-names[5]="farhan";
-names[6]="lili";
-names[7]="pooja";
-names[8]="lav";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
 names[9]="jim";
 
 
